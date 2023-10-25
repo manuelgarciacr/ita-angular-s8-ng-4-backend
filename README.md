c99408cf1b4186d632fb370c38588b7e3fc5441c
Manuel García Criado (ITA) <manuel.garcia.cr@hotmail.com>
2023-10-25 13:17:23 +0200

    Level 1 Exercises 1 & 2. Developing.

---
+++ README.md
@@ -0,0 +1,13 @@
+# IT Academy Sprint 8 Angular IV Inprocode. Backend
+
+## Level 1. Exercise 2.
+
+Nodejs server: Express
+BDD: MongoDB Community
+Mongo ODM: Mongoose
+Hashing: Bcryptjs
+Debug: Debug
+Logger: Morgan
+HTTP response headers security: Helmet
+
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,49 +1,66 @@
-const { Router} = require('express');
+const { Router } = require("express");
 const router = Router();
-const users = require("../models/User")
+const users = require("../models/User");
 
-router.get('/users', (req, res, next) => {
+router.get("/users", (req, res, next) => {
     const query = req.query;
     console.log("QUERY", req.query);
     users
         .find(query)
-        .then(data => {console.log(data); return data})
-        .then(data =>
+        .then((data) => {
+            console.log(data);
+            return data;
+        })
+        .then((data) =>
             res.send({
                 status: 200,
                 message: "Users retrieved",
                 data,
             })
-        ).catch(err => next(err));
+        )
+        .catch((err) => next(err));
 });
 
-router.put('/users', (req, res, next) => {
+router.put("/users", (req, res, next) => {
     console.log("PPUUTT", req.body, res.body);
     users
         .findByIdAndUpdate(req.body._id, req.body)
-        .then(data =>
+        .then((data) =>
             res.send({
                 status: 200,
                 message: "User updated",
                 data,
             })
         )
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }); // one user
 
-router.post('/users', (req, res, next) => {
+router.post("/users", (req, res, next) => {
     console.log("PPOOSSTT", req.body, res.body);
-    users.create(req.body)
-        .then(data =>
+    users
+        .create(req.body)
+        .then((data) =>
             res.send({
                 status: 200,
                 message: "User added",
                 data,
             })
         )
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }); // one user
 
-router.delete('/users/:id'); // one user
+router.delete("/users/:id", (req, res, next) => {
+    console.log("DDEELLEETTEE", req.body, res.body);
+    users
+        .deleteOne({ _id: req.params.id })
+        .then((data) =>
+            res.send({
+                status: 200,
+                message: "User deleted",
+                data,
+            })
+        )
+        .catch((err) => next(err));
+}); // one user
 
 module.exports = router;
