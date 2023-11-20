a384fa5af1dd2dda379d9bca215da94c2abceac3
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-11-20 23:18:46 +0100

    Level 1 Exercise 3. Map.

--- src/routes/features.js
+++ src/routes/features.js
@@ -4,7 +4,6 @@ const features = require("../models/Feature");
 
 router.get("/features", (req, res, next) => {
     const query = req.query;
-    console.log("QUERY FFFF", req.query);
     features
         .find(query)
         .then((data) => {
@@ -22,42 +21,39 @@ router.get("/features", (req, res, next) => {
 });
 
 router.put("/features", (req, res, next) => {
-    console.log("PPUUTT", req.body, res.body);
     features
         .findByIdAndUpdate(req.body._id, req.body)
         .then((data) =>
             res.send({
                 status: 200,
                 message: "Feature updated",
-                data,
+                data: [data],
             })
         )
         .catch((err) => next(err));
 }); // one user
 
 router.post("/features", (req, res, next) => {
-    console.log("PPOOSSTT FFFF", req.body, res.body);
     features
         .create(req.body)
         .then((data) =>
             res.send({
                 status: 200,
                 message: "Feature added",
-                data,
+                data: [data],
             })
         )
         .catch((err) => next(err));
 }); // one user
 
 router.delete("/features/:id", (req, res, next) => {
-    console.log("DDEELLEETTEE", req.body, res.body);
     features
         .deleteOne({ _id: req.params.id })
         .then((data) =>
             res.send({
                 status: 200,
                 message: "Feature deleted",
-                data,
+                data: data,
             })
         )
         .catch((err) => next(err));
