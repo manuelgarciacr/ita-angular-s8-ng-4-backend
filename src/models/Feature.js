4e915b83fc6dd5fa75293f941b700baa8c888095
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-11-24 20:21:21 +0100

    Level 2 Exercise 4.

--- src/models/Feature.js
+++ src/models/Feature.js
@@ -3,17 +3,21 @@ const {Schema, model, Types} = require('mongoose');
 // Workaround bug. An empty String causes validation error when the field is required
 Schema.Types.String.checkRequired(v => v != null);
 
-const featureSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
+const featureSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        feature: {
+            type: Schema.Types.Mixed,
+            required: true,
+        },
     },
-    feature: {
-        type: Schema.Types.Mixed,
-        required: true
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true
-});
+);
 
 module.exports = model('Feature', featureSchema);
